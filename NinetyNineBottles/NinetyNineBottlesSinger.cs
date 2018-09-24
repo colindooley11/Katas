84310409811b60c413800ce3ad7e99254f4b297c
Colin Dooley <colind@asos.com>
2018-09-24 18:17:13 +0100

    Added projects and solution

---
+++ NinetyNineBottles/NinetyNineBottlesSinger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NinetyNineBottles
+{
+    public class NinetyNineBottlesSinger
+    {
+        public string Verse(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Verses(int numberFrom, int numberTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Song()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ninetyninebottleskatatests.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NinetyNineBottlesKata
+{
+    [TestFixture]
+    public class NinetyNineBottlesKataTests
+    {
+    }
+}
